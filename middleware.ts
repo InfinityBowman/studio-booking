e4298de372e71596741154a9b67d12407e7473fc
Jacob Maynard <jacobamaynard@proton.me>
2025-02-26 15:45:31 -0700

    Configured for ec2

--- middleware.ts
+++ middleware.ts
@@ -22,6 +22,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/protected/:path*', // Protect /protected and any sub-routes
+    '/protected/:path*', // Protect route /protected and any sub-routes
   ],
 };
